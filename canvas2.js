5c62d54e215dafb4a00f74e817a8e6e49af28db2
Alessandro Stella <alessandro.stella2004@gmail.com>
2023-05-07 23:49:30 +0200

    Adding loading animation to 3d scene

--- canvas2.js
+++ canvas2.js
@@ -6,38 +6,63 @@ canvas.height = window.innerHeight;
 
 const context = canvas.getContext("2d");
 const frameCount = 534;
+let imageLoaded = 0;
+let imageToLoad = 603;
 
 const currentFrame = (folder, index) =>
     `./${folder}/frame${(index + 1).toString()}.png`;
 
+function createImage(src) {
+    const img = new Image();
+
+    img.src = src;
+
+    img.onload = function () {
+        imageLoaded++;
+
+        if (imageLoaded == imageToLoad) {
+            const hideContent = document.getElementById("hide-content");
+            const initialAnimationContainer =
+                document.getElementById("initialAnimation");
+
+            initialAnimationContainer.classList.remove("stopScroll");
+            hideContent.classList.add("hidden");
+        }
+    };
+
+    return img;
+}
+
 function addFrames(index, end = false) {
-    console.log(end);
     let imagesToAdd = [];
 
     for (let i = 0; i <= 9; i++) {
-        const img = new Image();
-        img.src = `./compressedResult/frame${index.toString()}-${i * 10}.png`;
-        imagesToAdd.push(img);
-        imagesToAdd.push(img);
+        const img = createImage(
+            `./compressedResult/frame${index.toString()}-${i * 10}.png`
+        );
+
+        imagesToAdd.push(img.cloneNode());
+        imagesToAdd.push(img.cloneNode());
     }
 
-    const fullText = new Image();
-    fullText.src = `./compressedResult/frame${index.toString()}-100.png`;
-    imagesToAdd.push(fullText);
-    imagesToAdd.push(fullText);
-    imagesToAdd.push(fullText);
-    imagesToAdd.push(fullText);
-    imagesToAdd.push(fullText);
+    const fullText = createImage(
+        `./compressedResult/frame${index.toString()}-100.png`
+    );
+
+    imagesToAdd.push(fullText.cloneNode());
+    imagesToAdd.push(fullText.cloneNode());
+    imagesToAdd.push(fullText.cloneNode());
+    imagesToAdd.push(fullText.cloneNode());
+    imagesToAdd.push(fullText.cloneNode());
 
     if (!end)
         for (let i = 9; i >= 0; i--) {
-            const img = new Image();
-            img.src = `./compressedResult/frame${index.toString()}-${
-                i * 10
-            }.png`;
-            console.log(`frame${index.toString()}-${i * 10}`);
-            imagesToAdd.push(img);
-            imagesToAdd.push(img);
+            const img = createImage(
+                `./compressedResult/frame${index.toString()}-${i * 10}.png`
+            );
+
+            imagesToAdd.push(img.cloneNode());
+            imagesToAdd.push(img.cloneNode());
         }
 
     enteringScene = [...enteringScene, ...imagesToAdd];
@@ -47,8 +72,6 @@ let enteringScene = [];
 let enteringSceneFrame = { frame: 0 };
 
 for (let i = 0; i < frameCount; i++) {
-    const img = new Image();
-
     switch (i + 1) {
         case 230:
             addFrames(i + 1);
@@ -67,7 +90,7 @@ for (let i = 0; i < frameCount; i++) {
             break;
 
         default:
-            img.src = currentFrame("compressedResult", i);
+            const img = createImage(currentFrame("compressedResult", i));
             enteringScene.push(img);
             break;
     }
